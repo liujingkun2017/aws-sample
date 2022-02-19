4f48018f71a57fe3e1b22c9f17c35c43a939f0e3
jingkunl <jingkunl@amazon.com>
2022-02-19 16:21:53 +0800

    update

--- cli/ec2-cli/sample3/delete_instance.py
+++ cli/ec2-cli/sample3/delete_instance.py
@@ -1,14 +1,13 @@
 import manage_instance
+import sys
 
 if __name__ == '__main__':
 
     print("delete instance")
 
-    stop_instanceIds = manage_instance.describe_stop_instances()
-    print("stop_instanceIds: ")
-    print(stop_instanceIds)
-
-    for instanceId in stop_instanceIds:
-        manage_instance.start_instance(instanceId)
-        print("start success: ")
-        print(instanceId)
+    if len(sys.argv) == 1:
+        print("fail")
+    else:
+        num = sys.argv[1]
+        for i in range(0, int(num)):
+            manage_instance.delete_instance("ec2-stack-" + str(i))
